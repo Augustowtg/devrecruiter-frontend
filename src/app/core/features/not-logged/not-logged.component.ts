29d26d3e012691d95a3babaa4907f3d3d2577c65
Augusto Wesley <62189499+Augustowtg@users.noreply.github.com>
2022-04-04 22:02:11 -0300

    Config ng lint in app

--- src/app/core/features/not-logged/not-logged.component.ts
+++ src/app/core/features/not-logged/not-logged.component.ts
@@ -1,15 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-not-logged',
   templateUrl: './not-logged.component.html',
   styleUrls: ['./not-logged.component.scss']
 })
-export class NotLoggedComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class NotLoggedComponent {
 }
