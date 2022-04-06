13a57b892c85f45276031ca56b8e7327dc5cae28
Gabriel da Silva Madeira <gabrielsmadeira@fac.pe.senac.br>
2022-04-05 21:06:19 -0300

    feat: /core/features/auth/ add auth component/module with pages login, register and auth service

---
+++ src/app/core/features/auth/auth.module.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { AuthComponent } from './auth.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+
+@NgModule({
+  declarations: [AuthComponent, LoginComponent, RegisterComponent],
+  imports: [CommonModule],
+})
+export class AuthModule {}
