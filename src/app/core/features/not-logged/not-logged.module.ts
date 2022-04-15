51913d7a2738589977f173bab71435ac0ab35e03
Augusto Wesley <62189499+Augustowtg@users.noreply.github.com>
2022-04-15 13:47:26 -0300

    Fix import landign pag

--- src/app/core/features/not-logged/not-logged.module.ts
+++ src/app/core/features/not-logged/not-logged.module.ts
@@ -2,14 +2,13 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
-import { LandingPageComponent } from './landing-page/landing-page.component';
 import { NotLoggedRoutingModule } from './not-logged-routing.module';
 import { NotLoggedComponent } from './not-logged.component';
 
 
 
 @NgModule({
-  declarations: [NotLoggedComponent, LandingPageComponent],
+  declarations: [NotLoggedComponent],
   imports: [
     CommonModule,
     NotLoggedRoutingModule,
